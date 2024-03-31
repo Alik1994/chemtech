933e077fab07f6561837c485348a6f179033da92
Albert Lavrinov <albert.lavr@gmail.com>
2024-03-31 18:17:30 +0300

    save changes

--- scripts/modules/sendEmail.js
+++ scripts/modules/sendEmail.js
@@ -1,18 +1,28 @@
-import { inputName, inputEmail, inputPhone, textarea } from "./validator.js";
+import {
+  inputName,
+  inputEmail,
+  inputPhone,
+  textarea,
+  submit,
+} from "./validator.js";
 
 const form = document.getElementById("contact-form");
 
 function sendEmail() {
   form.addEventListener("submit", function (event) {
+    inputName.value = "";
+    inputEmail.value = "";
+    inputPhone.value = "";
+    textarea.value = "";
+
+    submit.classList.add("btn_disabled");
+
     event.preventDefault();
     // these IDs from the previous steps
     emailjs.sendForm("service_contact", "contact_form", this).then(
       () => {
         console.log("SUCCESS!");
-        inputName.value = "";
-        inputEmail.value = "";
-        inputPhone.value = "";
-        textarea.value = "";
+        submit.classList.remove("btn_disabled");
       },
       (error) => {
         console.log("FAILED...", error);
